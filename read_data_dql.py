f6706d59a1c2ea2bd60a69dca25ad1ae9c0c8a94
Rishi <62651883+rushizirpe@users.noreply.github.com>
2023-05-05 16:47:30 +0530

    Update read_data_dql.py

--- read_data_dql.py
+++ read_data_dql.py
@@ -12,3 +12,28 @@ config = {
     'password' : db['password'], 
     'auth_plugin' : 'mysql_native_password'
 }
+
+def execute_sql_logic(_query):
+    """
+        This functions implements the DQL logic i.e., it runs SQL statements. 
+        It takes one input as an argument:
+            _query: a string of an SQL query
+    """
+
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    # cursor = connection.cursor( buffered=True , dictionary=True)
+    # _query = f'select * from actor limit 5;'
+    cursor.execute(_query)
+
+    columns = [col[0] for col in cursor.description]
+    frame = [dict(zip(columns,row)) for row in cursor.fetchall()]
+
+    df = pd.DataFrame(frame)
+    # df.set_index(df.columns[0], inplace=True)
+    cursor.close()
+    connection.close()
+
+    return df
+
+  
